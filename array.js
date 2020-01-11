29a4ff9f01eb983bb569603a5e149e3894d256f8
Kyomik <almrilhamm@gmail.com>
2020-01-11 15:27:50 +0100

    penggunaan function join

--- array.js
+++ array.js
@@ -1,10 +1,3 @@
-var length = prompt("Berapa Data yg ingin dimasukkan hari ini?");
+var arr = ['Kyoo', 'Luci', 'Miku'];
 
-var arr = [];
-
-for(var i = 0; i < length; i++){
-    arr[i] = prompt("Data " + (i+1) + " : ");
-}
-alert("Isi dari array : " + arr);
-alert("besar array nya : " + arr.length());
-
+console.log(arr.join('-'));
