5a7b3b163c22c9dd4ce24f9f66bd48a9ad22ad41
Kyomik <almrilhamm@gmail.com>
2020-01-11 14:35:47 +0100

    penggunaan functioan expressios dan deskripsion

--- function.js
+++ function.js
@@ -1,5 +1,16 @@
-function faktorial(n){
-    if(n === 0) return 1;
-    return n * faktorial(n-1);
+var ang1;
+var ang2;
+
+var penambahanEx = function(ang1, ang2){
+    return ang1 + ang2;
 }
-console.log("faktorial dari " + 10 + " : " + faktorial(5));
+
+ang1 = parseInt(prompt("Bil 1 : "));
+ang2 = parseInt(prompt("Bil 2 : "));
+
+alert(ang1 + "+" + ang2 + " = " + penambahanEx(ang1,ang2));
+alert(ang1 + "+" + ang2 + " = " + penambahanDes(ang1,ang2));
+
+function penambahanDes(ang1, ang2){
+    return ang1 + ang2;
+}
