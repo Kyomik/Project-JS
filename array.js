4bf674e2cfba7905b2463dad2e257797bd169c72
Kyomik <almrilhamm@gmail.com>
2020-01-12 02:58:33 +0100

    penambahan dan penghapusan isi array

--- array.js
+++ array.js
@@ -1,3 +1,21 @@
-var arr = ['Kyoo', 'Luci', 'Miku'];
+var arr = ['Kyoo', 'Luci', 'Nofa'];
 
-console.log(arr.join('-'));
+console.log(("Isi Array pada saat pertama kali : " + arr.join()));
+
+arr.push('Kuki', 'Cotto');
+
+console.log("Saat di manipulasi dgn method2 array : ");
+
+console.log((arr.join()));
+
+arr.pop();
+
+console.log((arr.join()));
+
+arr.unshift('Cerry', 'Riska');
+
+console.log((arr.join()));
+
+arr.shift();
+
+console.log((arr.join()));
